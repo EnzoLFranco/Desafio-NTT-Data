51697c7557f5c0c383ef356e47f57e3f34367d6f
EnzoLFranco <fenzolemos@gmail.com>
2025-08-05 15:16:33 -0300

    feat: Adiciona o método que cria os produtos

--- microservice-products/src/main/java/com/nttdata/products/microservice_products/controller/ProdutoController.java
+++ microservice-products/src/main/java/com/nttdata/products/microservice_products/controller/ProdutoController.java
@@ -1,7 +1,11 @@
 package com.nttdata.products.microservice_products.controller;
 
+import com.nttdata.products.microservice_products.model.Produto;
 import com.nttdata.products.microservice_products.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +16,9 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    
+    public ResponseEntity<Produto> createProduto(@RequestBody Produto produto){
+        Produto savedProduto = produtoRepository.save(produto);
+        return new ResponseEntity<>(savedProduto, HttpStatus.CREATED);
+    }
 
 }
