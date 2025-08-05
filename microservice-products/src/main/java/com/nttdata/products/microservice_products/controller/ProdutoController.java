94c2b0e314e5baf00abdc3c1fff19452a657bb52
EnzoLFranco <fenzolemos@gmail.com>
2025-08-05 15:14:27 -0300

    feat: Instancia o repository e aplica o Autowired

--- microservice-products/src/main/java/com/nttdata/products/microservice_products/controller/ProdutoController.java
+++ microservice-products/src/main/java/com/nttdata/products/microservice_products/controller/ProdutoController.java
@@ -1,9 +1,17 @@
 package com.nttdata.products.microservice_products.controller;
 
+import com.nttdata.products.microservice_products.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    
+
 }
