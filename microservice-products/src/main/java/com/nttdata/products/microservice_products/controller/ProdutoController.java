8d1596215303ec93008b74463af3c11d16d26ae4
EnzoLFranco <fenzolemos@gmail.com>
2025-08-05 15:26:05 -0300

    feat: Adiciona o método para listar todos os produtos

--- microservice-products/src/main/java/com/nttdata/products/microservice_products/controller/ProdutoController.java
+++ microservice-products/src/main/java/com/nttdata/products/microservice_products/controller/ProdutoController.java
@@ -5,9 +5,9 @@ import com.nttdata.products.microservice_products.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
@@ -16,9 +16,16 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    @PostMapping
     public ResponseEntity<Produto> createProduto(@RequestBody Produto produto){
         Produto savedProduto = produtoRepository.save(produto);
         return new ResponseEntity<>(savedProduto, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> getAllProdutos(){
+        List<Produto> produtos = produtoRepository.findAll();
+        return new ResponseEntity<>(produtos, HttpStatus.OK);
     }
 
 }
