7ce802ab0498eb517b86e83164c7f614cfb0abac
EnzoLFranco <fenzolemos@gmail.com>
2025-08-06 15:24:11 -0300

    feat: Adiciona o projeto para o microsserviço de pedidos

---
+++ microservice-orders/src/test/java/com/nttdata/orders/microservice_orders/MicroserviceOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.orders.microservice_orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
