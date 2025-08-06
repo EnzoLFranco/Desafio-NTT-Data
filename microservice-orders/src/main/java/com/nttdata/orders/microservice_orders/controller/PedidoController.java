58b1b72bd3cda863258eb127f8a4ad6ae9dd8aba
EnzoLFranco <fenzolemos@gmail.com>
2025-08-06 15:49:22 -0300

    feat: Instancia o WebClientBuilder e adiciona o construtor

--- microservice-orders/src/main/java/com/nttdata/orders/microservice_orders/controller/PedidoController.java
+++ microservice-orders/src/main/java/com/nttdata/orders/microservice_orders/controller/PedidoController.java
@@ -1,10 +1,20 @@
 package com.nttdata.orders.microservice_orders.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @RequestMapping("/pedidos")
 public class PedidoController {
-    
+
+    private final WebClient.Builder webClientBuilder;
+
+    @Autowired
+    public PedidoController(WebClient.Builder webClientBuilder){
+        this.webClientBuilder = webClientBuilder;
+    }
+
+
 }
