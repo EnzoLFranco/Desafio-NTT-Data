b49e3a1b8a6b42a5442e8240633300d5440580d6
EnzoLFranco <fenzolemos@gmail.com>
2025-08-06 15:19:18 -0300

    feat: Adiciona o método de filtro para validar o token

--- api-gateway/src/main/java/com/nttdata/gateway/api_gateway/filter/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/nttdata/gateway/api_gateway/filter/AuthenticationFilter.java
@@ -1,8 +1,12 @@
 package com.nttdata.gateway.api_gateway.filter;
 
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @Component
 public class AuthenticationFilter implements GlobalFilter, Ordered {
@@ -10,6 +14,28 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
     private static final String AUTH_HEADER = "Authorization";
     private static final String FIXED_TOKEN = "Bearer token-nttdata";
 
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain){
+        String path = exchange.getRequest().getURI().getPath();
+
+        if(path.startsWith("/h2-console")){
+            return chain.filter(exchange);
+        }
+
+        if (!exchange.getRequest().getHeaders().containsKey(AUTH_HEADER)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String token = exchange.getRequest().getHeaders().get(AUTH_HEADER).get(0);
+
+        if (!FIXED_TOKEN.equals(token)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
 
 
 
