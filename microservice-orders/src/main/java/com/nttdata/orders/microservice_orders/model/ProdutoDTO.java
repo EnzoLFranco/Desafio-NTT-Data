45820e356218755db83ecf2d7cc2b0267d5f28c3
EnzoLFranco <fenzolemos@gmail.com>
2025-08-06 15:53:54 -0300

    feat: Define as annotations para a classe Produto e seus atributos

--- microservice-orders/src/main/java/com/nttdata/orders/microservice_orders/model/ProdutoDTO.java
+++ microservice-orders/src/main/java/com/nttdata/orders/microservice_orders/model/ProdutoDTO.java
@@ -1,4 +1,11 @@
 package com.nttdata.orders.microservice_orders.model;
 
+import lombok.Data;
+
+@Data
 public class ProdutoDTO {
+    private Long id;
+    private String nome;
+    private String descricao;
+    private double preco;
 }
