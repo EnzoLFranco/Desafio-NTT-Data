4343599813cc7736f7dc562d3757b0f3bb2fa49f
EnzoLFranco <fenzolemos@gmail.com>
2025-08-06 15:44:26 -0300

    feat: Define as annotations para a classe Pedido e seus atributos

--- microservice-orders/src/main/java/com/nttdata/orders/microservice_orders/model/PedidoDTO.java
+++ microservice-orders/src/main/java/com/nttdata/orders/microservice_orders/model/PedidoDTO.java
@@ -1,4 +1,16 @@
 package com.nttdata.orders.microservice_orders.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PedidoDTO {
+
+    private String cliente;
+    private List<ItemPedidoDTO> itens;
 }
