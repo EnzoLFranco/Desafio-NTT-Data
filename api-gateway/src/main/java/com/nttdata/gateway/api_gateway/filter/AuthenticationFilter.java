97d84e451a4ae9aad74f0f80fdb8fb8450c92589
EnzoLFranco <fenzolemos@gmail.com>
2025-08-06 15:15:46 -0300

    feat: Implementa as interfaces no filter e cria a header/token

--- api-gateway/src/main/java/com/nttdata/gateway/api_gateway/filter/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/nttdata/gateway/api_gateway/filter/AuthenticationFilter.java
@@ -1,7 +1,16 @@
 package com.nttdata.gateway.api_gateway.filter;
 
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AuthenticationFilter {
+public class AuthenticationFilter implements GlobalFilter, Ordered {
+
+    private static final String AUTH_HEADER = "Authorization";
+    private static final String FIXED_TOKEN = "Bearer token-nttdata";
+
+
+
+
 }
