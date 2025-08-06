8450be02c705c4bc8b814c40b635f2ebc37a9e24
EnzoLFranco <fenzolemos@gmail.com>
2025-08-06 15:54:48 -0300

    feat: Adiciona o endpoint para criar pedido

--- microservice-orders/src/main/java/com/nttdata/orders/microservice_orders/controller/PedidoController.java
+++ microservice-orders/src/main/java/com/nttdata/orders/microservice_orders/controller/PedidoController.java
@@ -1,9 +1,15 @@
 package com.nttdata.orders.microservice_orders.controller;
 
+import com.nttdata.orders.microservice_orders.model.PedidoDTO;
+import com.nttdata.orders.microservice_orders.model.ProdutoDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/pedidos")
@@ -16,5 +22,19 @@ public class PedidoController {
         this.webClientBuilder = webClientBuilder;
     }
 
+    @PostMapping
+    public Mono<ResponseEntity<String>> criarPedido(@RequestBody PedidoDTO pedido) {
+        WebClient webClient = webClientBuilder.baseUrl("http://microservice-products").build();
 
+        Mono<ProdutoDTO> produtoMono = webClient.get()
+                .uri("/produtos/{id}", pedido.getItens().get(0).getIdProduto())
+                .retrieve()
+                .bodyToMono(ProdutoDTO.class);
+
+        return produtoMono.map(produto -> {
+            System.out.println("Pedido recebido para o cliente: " + pedido.getCliente());
+            System.out.println("Item do pedido: " + produto.getNome() + ", quantidade: " + pedido.getItens().get(0).getQuantidade());
+            return ResponseEntity.ok("Pedido para " + pedido.getCliente() + " simulado com sucesso!");
+        }).defaultIfEmpty(ResponseEntity.notFound().build());
+    }
 }
