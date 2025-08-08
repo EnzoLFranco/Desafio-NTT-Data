92efb2b72079093d2cf374aff8e0c3b8f9cbeea9
EnzoLFranco <fenzolemos@gmail.com>
2025-08-08 16:40:46 -0300

    feat: Habilita o DiscoveryClient no microsserviço de Pedidos e adiciona o WebClient como Bean

--- microservice-orders/src/main/java/com/nttdata/orders/microservice_orders/MicroserviceOrdersApplication.java
+++ microservice-orders/src/main/java/com/nttdata/orders/microservice_orders/MicroserviceOrdersApplication.java
@@ -2,12 +2,23 @@ package com.nttdata.orders.microservice_orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceOrdersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceOrdersApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public WebClient.Builder webClientBuilder() {
+		return WebClient.builder();
 	}
 
 }
