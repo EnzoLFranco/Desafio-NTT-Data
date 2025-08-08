6061be8aa2007abb67082d53fd1b4bd42e315f95
EnzoLFranco <fenzolemos@gmail.com>
2025-08-08 16:41:32 -0300

    fix: Corrige a url dos produtos e corrige o mapeamento dos pedidos

--- microservice-orders/src/main/java/com/nttdata/orders/microservice_orders/controller/PedidoController.java
+++ microservice-orders/src/main/java/com/nttdata/orders/microservice_orders/controller/PedidoController.java
@@ -1,7 +1,6 @@
 package com.nttdata.orders.microservice_orders.controller;
 
-import com.nttdata.orders.microservice_orders.model.PedidoDTO;
-import com.nttdata.orders.microservice_orders.model.ProdutoDTO;
+import com.nttdata.orders.microservice_orders.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,26 +14,24 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/pedidos")
 public class PedidoController {
 
-    private final WebClient.Builder webClientBuilder;
+    private final WebClient webClient;
 
     @Autowired
-    public PedidoController(WebClient.Builder webClientBuilder){
-        this.webClientBuilder = webClientBuilder;
+    public PedidoController(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("http://microservice-products").build();
     }
 
     @PostMapping
     public Mono<ResponseEntity<String>> criarPedido(@RequestBody PedidoDTO pedido) {
-        WebClient webClient = webClientBuilder.baseUrl("http://microservice-products").build();
-
-        Mono<ProdutoDTO> produtoMono = webClient.get()
+        return this.webClient.get()
                 .uri("/produtos/{id}", pedido.getItens().get(0).getIdProduto())
                 .retrieve()
-                .bodyToMono(ProdutoDTO.class);
-
-        return produtoMono.map(produto -> {
-            System.out.println("Pedido recebido para o cliente: " + pedido.getCliente());
-            System.out.println("Item do pedido: " + produto.getNome() + ", quantidade: " + pedido.getItens().get(0).getQuantidade());
-            return ResponseEntity.ok("Pedido para " + pedido.getCliente() + " simulado com sucesso!");
-        }).defaultIfEmpty(ResponseEntity.notFound().build());
+                .bodyToMono(ProdutoDTO.class)
+                .map(produto -> {
+                    System.out.println("Pedido recebido para o cliente: " + pedido.getCliente());
+                    System.out.println("Item do pedido: " + produto.getNome() + ", quantidade: " + pedido.getItens().get(0).getQuantidade());
+                    return ResponseEntity.ok("Pedido para " + pedido.getCliente() + " simulado com sucesso!");
+                })
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
